c65006ea91db89a4425f16ef215a82c1e3b5a65d
rusherrg <rushang101@gmail.com>
2019-04-14 17:38:54 +0530

    Reviews Added

--- Dashboard/app/static/reviews.js
+++ Dashboard/app/static/reviews.js
@@ -2,3 +2,31 @@ revs = document.getElementsByClassName('review_text');
 for(i=0;i<revs.length;i++){
     revs[i].innerHTML = revs[i].innerHTML.replace(/\n/g,'<br/>');
 }
+
+ratings = document.getElementsByClassName('rating');
+for(i=0;i<ratings.length;i++){
+    star = parseInt(ratings[i].innerHTML);
+    stars = "";
+    for(j=0;j<star;j++){
+        stars += "<i class='material-icons'>star</i>";
+    }
+    if(star!=parseFloat(ratings[i].innerHTML))
+        stars += "<i class='material-icons'>half_star</i>";
+    ratings[i].innerHTML = stars; 
+}
+
+
+var url = window.location.href;
+url = new URL(url);
+var type = url.searchParams.get('type');
+var page = url.searchParams.get('page');
+var classs = url.searchParams.get('class');
+
+function change_page(dir){
+    url = window.location.origin + window.location.pathname;
+    url = url + '?type=' + type + '&page=' + String(parseInt(page) + dir);
+    if(classs){
+        url += '&class=' + classs;
+    }
+    window.open(url,'_self');
+}
--- Dashboard/app/views.py
+++ Dashboard/app/views.py
@@ -1,5 +1,5 @@
 # views.py
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify, request, redirect
 from bson import ObjectId
 
 from app import app
@@ -13,8 +13,19 @@ def index():
 @app.route('/reviews')
 def reviews():
     type_rev = request.args.get('type')
-    page = int(request.args.get('page'))
-    return render_template("reviews.html", reviews=allreviews[type_rev][10*(page-1):10*page])
+    page = max(1, int(request.args.get('page')))
+    print(page)
+    if type_rev in ['all_reviews', 'most_recent_reviews']:
+        return render_template("reviews.html", reviews=allreviews[type_rev][10*(page-1):10*page])
+    elif type_rev in ['battery', 'picture', 'sound', 'fingerprint', 'value']:
+        classs = request.args.get('class')
+        reviews = []
+        for i in allreviews[type_rev][classs]:
+            reviews.append(allreviews['all_reviews'][i])
+        return render_template("reviews.html", reviews=reviews[10*(page-1):10*page])
+    else:
+        return redirect('index')
+
 
 @app.route('/about')
 def about():
